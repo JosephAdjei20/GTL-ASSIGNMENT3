9c026833db902e3dca2cdefb4895c88d3934ef96
JosephGtl <68908267+JosephGtl@users.noreply.github.com>
2020-08-13 08:59:20 -1200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import JhoEy from './week-3/Joseph.js';
+
+import './week-3/css/components.css';
+import './week-3/css/global.css';
+import './week-3/css/layout.css';
+import './week-3/css/sample-wallpaper.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <JhoEy/>
+      </div>
+  );
+}
+
+export default App;
